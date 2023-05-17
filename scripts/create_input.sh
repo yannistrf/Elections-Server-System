0c22a0687b818d30210cc21069683c0f2071d196
yannistrf <yannistrf@gmail.com>
2023-05-17 16:01:46 +0300

    Created create_input.sh script and politicalParties.txt

---
+++ scripts/create_input.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Check for args num
+if [[ $# != 2 ]]; then
+    echo "Usage: ./create_input.sh [politicalParties.txt] [numLines]"
+    exit 1
+fi
+
+# Extract args
+partiesFile=$1
+numLines=$2
+
+# Check if the partiesFile exists
+if [ ! -f $partiesFile ]; then
+    echo "$partiesFile does not exist."
+    exit 1
+fi
+
+# Check if numLines > 0
+if [[ $numLines -lt 1 ]]; then
+    echo "Please enter a positive number."
+    exit 1
+fi
+
+
+# Get number of parties (number of lines of partiesFile)
+partiesNum=`wc -l < $partiesFile`
+
+# If inputFile exists, delete it
+if [ -f "inputFile" ]; then
+    rm -f inputFile
+fi
+
+
+for (( lineNum=1; lineNum<=numLines; lineNum++ ));
+do
+    # Pick a random party
+    partyNum=$(($RANDOM % partiesNum + 1))
+    randParty=`sed -n "${partyNum}p" < $partiesFile`
+    
+    # Generate first and last name
+    randLen=$(($RANDOM % 10 + 3))
+    firstName=`cat /dev/urandom | tr -dc "a-z" | fold -w $randLen| head -n 1`
+    randLen=$(($RANDOM % 10 + 3))
+    lastName=`cat /dev/urandom | tr -dc "a-z" | fold -w $randLen| head -n 1`
+
+    # Write the record in the inputFile
+    echo "$firstName $lastName $randParty" >> inputFile
+done
